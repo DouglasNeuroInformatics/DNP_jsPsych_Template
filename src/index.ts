a74427d687c2fe09a14c4a4c5b3a75115331cef4
Cian Monnin <cianmonnin@gmail.com>
2025-01-29 18:51:48 -0500

    refactor: change implementation of i18n for Translator. Added correct validation in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import { experimentSettingsJson } from "./experimentSettings.ts";
 import { jsPsychExperiment } from "./jsPsychExperiment.ts";
-import { $ExperimentResults } from "./schemas.ts";
+import { $ExperimentResults, $Settings } from "./schemas.ts";
 
 import type { Language } from "/runtime/v1/@opendatacapture/runtime-core";
 
 import "/runtime/v1/jspsych@8.x/css/jspsych.css";
 
 import { defineInstrument } from "/runtime/v1/@opendatacapture/runtime-core";
+import { z } from "/runtime/v1/zod@3.23.x";
 
 export default defineInstrument({
   kind: "INTERACTIVE",
-  // if multilingual experimentSettingsJson needs a language field 
+  // if multilingual experimentSettingsJson needs a language field
   language: experimentSettingsJson.language as Language,
   internal: {
     edition: 1,
@@ -19,6 +20,15 @@ export default defineInstrument({
   tags: ["interactive", "jsPysch", "PLACEHOLDER"],
   content: {
     async render(done) {
+      const settingsParseResult = $Settings.safeParse(experimentSettingsJson);
+
+      // parse settings
+      if (!settingsParseResult.success) {
+        throw new Error("validation error, check experiment settings", {
+          cause: settingsParseResult.error,
+        });
+      }
+
       await jsPsychExperiment(done);
     },
   },
@@ -31,5 +41,9 @@ export default defineInstrument({
     title: "<PLACEHOLDER>",
   },
   measures: {},
-  validationSchema: $ExperimentResults,
+  validationSchema: z.object({
+    version: z.string(),
+    timestamp: z.string(),
+    experimentResults: z.array($ExperimentResults),
+  }),
 });
