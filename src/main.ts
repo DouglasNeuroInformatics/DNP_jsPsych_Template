f6b3436bd4915153f87585aaa58b392d587a71e3
Cian <cianmonnin@gmail.com>
2025-01-30 16:32:30 -0500

    fix: added trasnaltor to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-import { jsPsychExperiment } from "./jsPsychExperiment";
+import { experimentSettingsJson } from "./experimentSettings.ts";
+import { jsPsychExperiment } from "./jsPsychExperiment.ts";
+import { $Settings } from "./schemas.ts";
+import { translator } from "./translations.ts";
+
+import type { Language } from "/runtime/v1/@opendatacapture/runtime-core";
 
 import "/runtime/v1/jspsych@8.x/css/jspsych.css";
 
+const settingsParseResult = $Settings.safeParse(experimentSettingsJson);
+
+// parse settings
+if (!settingsParseResult.success) {
+  throw new Error("validation error, check experiment settings", {
+    cause: settingsParseResult.error,
+  });
+}
+translator.init();
+translator.changeLanguage(settingsParseResult.data.language as Language);
 await jsPsychExperiment();
